77bbbb672114a8d7999f44f4829159e9d7cc299e
Bradley Myers <myers.bradley@hotmail.com>
2020-10-07 14:37:20 -0400

    Added bootstrap and a layout file

--- App.py
+++ App.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
-def Hello():
-    return "Hello World"
+def Main():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.debug = True
