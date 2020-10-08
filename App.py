e8ef4ef57fadc319e66f1795a572996773fb8670
Bradley Myers <myers.bradley@hotmail.com>
2020-10-08 10:00:13 -0400

    Added database and user class

--- App.py
+++ App.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+
 Bootstrap(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique = True)
+    password = db.Column(db.String, nullable = False)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return 'User({}, {})'.format(self.username, self.email)
 
 @app.route('/')
 def Main():
