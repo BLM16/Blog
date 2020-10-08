2340060ff81be3ecb548e7bf8ac05d52d645c57e
Bradley Myers <myers.bradley@hotmail.com>
2020-10-07 21:29:56 -0400

    Added login & register pages. No functionality.

--- App.py
+++ App.py
@@ -8,6 +8,14 @@ Bootstrap(app)
 def Main():
     return render_template("index.html")
 
+@app.route('/login')
+def Login():
+    return render_template("login.html")
+
+@app.route('/register')
+def Register():
+    return render_template("register.html")
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
